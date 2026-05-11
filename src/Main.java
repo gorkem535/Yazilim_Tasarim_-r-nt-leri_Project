c5531618c4c718d5c56a0143be27dc14eb98be65
gorkem535 <gorkemisik125@gmail.com>
2026-05-11 20:12:25 +0300

    eat: integrate Phase 2 structural patterns into game loop

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.List;
 import java.util.ArrayList;
 
 public class Main {
@@ -5,15 +6,24 @@ public class Main {
 	public static void main(String[] args) {
 	ArrayList<Game>list=new ArrayList<>();
 	
-     list.add(Game_ObjectFactory.createGameObject("Main Charecter", 1.80, 70.0, 0));
+    Game player=Game_ObjectFactory.createGameObject("Main Charecter", 1.80, 70.0, 0);
+    Game armoredPlayer=new ArmoredDecorator(player);
+    list.add(armoredPlayer);
      list.add(Game_ObjectFactory.createGameObject("Enemy", 1.75,65.0 , 0));
      list.add(Game_ObjectFactory.createGameObject("Health_Cure", 0, 0, 10));
       
+     LegacyBoss oldBoss=new LegacyBoss();
+     Game adaptedBoss=new LegacyBoss_Adapter(oldBoss);
+     list.add(adaptedBoss);
      
     for (Game obj : list) {
 		obj.render();
 	}
-
+    
+   
+    
+   
 	}
+	
 
 }
